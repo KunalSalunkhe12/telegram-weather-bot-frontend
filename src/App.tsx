00f557cbe7d4e55827e58fa3624821b329f144c8
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2024-01-29 19:53:57 +0530

    Update user fetch interval to 5 minutes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "@/components/shared/Navbar";
 import { getUsers } from "./api";
 
-// Get users every 15 minutes to avoid server sleeping due to inactivity
+// Get users every 5 minutes to avoid server sleeping due to inactivity
 // Prevent server cold start as it is hosted on Render's free tier
-setInterval(async () => {
-  await getUsers();
+setInterval(() => {
+  console.log("Render server pinged");
+  getUsers();
 }, 1000 * 60 * 5);
 
 function App() {
